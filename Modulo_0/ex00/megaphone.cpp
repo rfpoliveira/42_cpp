df3d9a6b5e19148a95db23fa38dbbe541bd6f0b0
rfpoliveira <rfpoliveira18@gmail.com>
2025-06-11 11:47:28 +0100

    adding ex00

---
+++ Modulo_0/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpedrosa <rpedrosa@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 14:12:54 by rpedrosa          #+#    #+#             */
+/*   Updated: 2025/06/09 14:12:55 by rpedrosa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBERABLE NOISE *";
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
+	return (0);
+}
